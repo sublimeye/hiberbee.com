708be0ea9c5aa42c96df9b57a3a60d73a6aaf79c
Vlad Volkov <vlad@hiberbee.com>
2018-01-02 13:57:13 +0200

    Fixed sonar issues. Fix for composer deps install in Travis

--- src/Kernel.php
+++ src/Kernel.php
@@ -23,6 +23,8 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
+    private const ROUTE_CONFIG_DIRECTORY_NAME = 'routes';
+
     const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
     public function getCacheDir()
@@ -60,12 +62,12 @@ class Kernel extends BaseKernel
     protected function configureRoutes(RouteCollectionBuilder $routes)
     {
         $confDir = $this->getProjectDir() . '/config';
-        if (is_dir($confDir . '/routes/')) {
+        if (is_dir($confDir . DIRECTORY_SEPARATOR . self::ROUTE_CONFIG_DIRECTORY_NAME . DIRECTORY_SEPARATOR)) {
             $routes->import($confDir . '/routes/*' . self::CONFIG_EXTS, '/', 'glob');
         }
-        if (is_dir($confDir . '/routes/' . $this->environment)) {
-            $routes->import($confDir . '/routes/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
+        if (is_dir($confDir . DIRECTORY_SEPARATOR . self::ROUTE_CONFIG_DIRECTORY_NAME . DIRECTORY_SEPARATOR . $this->environment)) {
+            $routes->import($confDir . DIRECTORY_SEPARATOR . self::ROUTE_CONFIG_DIRECTORY_NAME . DIRECTORY_SEPARATOR . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
         }
-        $routes->import($confDir . '/routes' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . DIRECTORY_SEPARATOR . self::ROUTE_CONFIG_DIRECTORY_NAME . self::CONFIG_EXTS, '/', 'glob');
     }
 }
